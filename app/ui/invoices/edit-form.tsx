4ce848e714f35990795350e363ee1baa0c9ee870
glitzypanic <jose.fariasdv@gmail.com>
2024-07-11 17:48:43 -0400

    refactor: Update invoice form to use updateInvoice function and fix initial state

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,7 +9,7 @@ import {
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { Button } from "@/app/ui/button";
-import { UpdateInvoice } from "./buttons";
+import { updateInvoice } from "@/app/lib/actions";
 import { useActionState } from "react";
 
 export default function EditInvoiceForm({
@@ -19,8 +19,8 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
-  const initialState = <div>null</div>;
-  const updateInvoiceWithId = UpdateInvoice.bind(null, { id: invoice.id });
+  const initialState = { message: "", errors: {} };
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
   const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
 
   return (
