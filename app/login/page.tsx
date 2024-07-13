0bef58943d7c5f0b2bc12c02a111353abb2bc1d8
glitzypanic <jose.fariasdv@gmail.com>
2024-07-13 19:13:23 -0400

    refactor: Update login page to include metadata for SEO

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,11 @@
 import AcmeLogo from "@/app/ui/acme-logo";
 import LoginForm from "@/app/ui/login-form";
 import type { NextAuthConfig } from "next-auth";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login",
+};
 
 export const authConfig = {
   pages: {
